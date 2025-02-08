a03f69105a64b721ac097e352c8236a74439b2a3
nanack12 <ananack12@gmail.com>
2025-02-08 12:26:01 +0000

    도커 환경세팅 수정 - 리액트 핫로딩 안되던 오류 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      protocol: 'ws',
+      host: '0.0.0.0',  
+      clientPort: 5173,   
+    },
+  },
+});
